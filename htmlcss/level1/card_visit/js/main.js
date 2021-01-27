74911754e614b361e49f8d70722553b4b039b258
dovietthanh <dovietthanh105@gmail.com>
2021-01-27 10:07:30 +0700

    card visit

---
+++ htmlcss/level1/card_visit/js/main.js
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>Facebook - Đăng nhập hoặc đăng kí</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="Style_card.css">
+<style>
+
+
+</style>
+<script src=""></script>
+<body>
+
+<div class="wapper">
+    <div class="about-inner">
+        <div class="image">
+            <img src="https://baohothuonghieu.com/banquyen/images/news/1536893974-QR%20CODE%20L%C3%80%20G%C3%8C-sblaw.jpeg">
+        </div>
+        <div class="vertical-line"></div>
+        <div class="about-content">
+            <div class="about-description">
+                <div class="tittle">
+                    <h2 class="name">LE VAN HUNG</h2>
+                    <p class="position">Cheif Excutive Officer</p>
+                </div>
+                <div class="about-list-infomation">
+                   <div class="about-items">
+                       <p>Phone:</p>
+                       <p>Email:</p>
+                       <p>Website:</p>
+                       <p>Address:</p>
+                   </div>
+                    <div class="info">
+                        <p>(+84)981787638</p>
+                        <p>contact@v68.vn</p>
+                        <p>www.v68.vn * www.aneed.vn</p>
+                        <p>No.6A,332 Lane Linh Nam, Hoang Mai, Hanoi</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
